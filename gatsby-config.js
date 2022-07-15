255a9774857f748131949875ae7727b102964263
Olivier Ramier <olivier.ramier@gmail.com>
2022-07-15 14:18:22 +0200

    remove offline mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         icon: `src/images/favicon.svg`,
       },
     },
-    "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     {
       resolve: `gatsby-plugin-plausible`,
       options: {
